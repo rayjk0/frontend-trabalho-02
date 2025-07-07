951ad0f007190d728b623f15877fd984de7b36a3
rayjk0 <raykaneressantos@gmail.com>
2025-07-07 11:00:47 -0300

    Base

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Pagina from './Pagina.tsx';
+import './Pagina.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode><Pagina /></React.StrictMode>
+);
+
