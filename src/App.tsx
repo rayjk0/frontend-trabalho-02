b12b195b81e14a2a8255fa66b9e5494abe524e29
rayjk0 <raykaneressantos@gmail.com>
2025-07-16 00:17:01 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PaginaCatalogo from './PaginaCatalogo';
 import LivroSelecionado from './LivroSelecionado';
+import PaginaCadastro from './PaginaCadastro'; 
 
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<PaginaCatalogo />} />
       <Route path="/livros/:id" element={<LivroSelecionado />} />
+      <Route path="/cadastro" element={<PaginaCadastro />}/>
     </Routes>
   );
 }
